7b63ab8323e2471d3690ffdf0c6cc3981898a6bc
Sihle Ndlovu <sihle@synthesis.co.za>
2025-11-11 23:31:48 +0200

    ami

--- main.tf
+++ main.tf
@@ -11,8 +11,23 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami           = var.ami
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
 
   tags = {
