c2d2c24050a9c3ee63fc00d40d1f4ecc015004ce
King-Zingelwayo <ndlovu.dev@outlook.com>
2025-11-11 22:23:30 +0200

    Add/Update Terraform pipeline configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-King-Zingelwayo-test-pipeline-app-07540"
+    bucket         = "terraform-state-king-zingelwayo-test-pipeline-app-40805"
     key            = "terraform.tfstate"
     region         = "eu-west-1"
     use_lockfile   = true
